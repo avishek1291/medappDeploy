7e5fb088c1146db0bb745f3d828046804b2fc0d7
Avishek <choudhury.avishek1@tcs.com>
2020-02-15 22:46:00 +0530

    deploy for heroku

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const path =  require('path');
 app.use(bodyparser.json());
 
 app.use('/', express.static(path.join(__dirname, 'public/med-app')));
-app.listen(3000, () => {
+app.listen(process.env.PORT|| 3000, () => {
      console.log('server started at port: 3000');
  })
