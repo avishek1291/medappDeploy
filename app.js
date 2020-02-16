bd379b37e908aa3e9802857c14e551f57ab8e827
Avishek <choudhury.avishek1@tcs.com>
2020-02-16 11:39:58 +0530

    minor change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app =  express();
 const path =  require('path');
 app.use(bodyparser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public/med-app')));
+app.use('/', express.static(path.join(__dirname, 'public/build')));
 app.listen(process.env.PORT|| 3000, () => {
      console.log('server started at port: 3000');
  })
