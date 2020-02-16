87c1ffdedf5490ed1bc167517889c774147120bd
Avishek <choudhury.avishek1@tcs.com>
2020-02-16 11:38:37 +0530

    added build

---
+++ public/build/precache-manifest.50a3c53521850ac6ea174ebdac166e20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "989ccf958fca895bb18f",
+    "url": "/static/css/main.498c9f9f.chunk.css"
+  },
+  {
+    "revision": "989ccf958fca895bb18f",
+    "url": "/static/js/main.989ccf95.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8e3938f92a9e9f5f7e70",
+    "url": "/static/js/2.8e3938f9.chunk.js"
+  },
+  {
+    "revision": "6bca95b21f3c13bc73de355f02ccea8a",
+    "url": "/index.html"
+  }
+];
